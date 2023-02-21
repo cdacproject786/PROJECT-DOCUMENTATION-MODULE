999c68219c952acdb9da295d1f3103e651bea806
Sayed Arbaaz <arbaazsayed1602@gmail.com>
2023-02-21 20:22:02 +0530

    security question table created

---
+++ database/Tables/Test tables/Security_Questions_Test_Table.sql
@@ -0,0 +1,10 @@
+create table Security_Questions_Test_Table
+ (
+ Security_Question_Id int(10) Primary key ,
+ Security_Question varchar(50) ,
+ Last_Updated_Date datetime,
+ Ext_Col_1 int(6),
+ Ext_Col_2 varchar(20),
+ Ext_Col_3 varchar(50) 
+ );
+

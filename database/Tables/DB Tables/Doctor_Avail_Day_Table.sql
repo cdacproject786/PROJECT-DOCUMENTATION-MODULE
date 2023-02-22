c9556334a0fdc1fe1ab9249f6329c12de54ca23e
Mirza Adnan Baig Nusratullah <118421209+MAdnanB@users.noreply.github.com>
2023-02-22 19:08:15 +0530

    Omkar restart it please

--- database/Tables/DB Tables/Doctor_Avail_Day_Table.sql
+++ database/Tables/DB Tables/Doctor_Avail_Day_Table.sql
@@ -3,8 +3,7 @@
 
 
 create table Doctor_Avail_Day_Table(
-Doc_Avail_DId int(10) primary key,
-last_Updated datetime default now() NOT NULL,
+Doc_Avail_DId int(10) primary key auto_increment,
 Mon boolean NOT NULL,
 Tue boolean NOT NULL,
 Wed boolean NOT NULL,
@@ -12,8 +11,10 @@ Thu boolean NOT NULL,
 Fri boolean NOT NULL,
 Sat boolean NOT NULL,
 Sun boolean NOT NULL,
+last_Updated datetime default now() NOT NULL,
 Extra_Col1 varchar(200) NULL,
 Extra_Col2 varchar(200) NULL,
 Extra_Col3 varchar(200) NULL
 );
+alter table Doctor_Avail_Day_Table Auto_Increment = 10001;
 

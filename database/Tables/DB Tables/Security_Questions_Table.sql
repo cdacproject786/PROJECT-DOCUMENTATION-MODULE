a77d716f08187321bd1740b6357ec3575a659758
Sayed Arbaaz <arbaazsayed1602@gmail.com>
2023-02-22 12:34:32 +0530

    security-questions-table-default timstamp added

--- database/Tables/DB Tables/Security_Questions_Table.sql
+++ database/Tables/DB Tables/Security_Questions_Table.sql
@@ -2,7 +2,7 @@ create table Security_Questions_Table
  (
  Security_Questions_Id int(10) Primary key ,
  Security_Question varchar(50) ,
- Last_Updated_Date datetime,
+ Last_Updated_Date datetime default now(),
  Ext_Col_1 int(6),
  Ext_Col_2 varchar(20),
  Ext_Col_3 varchar(50) 

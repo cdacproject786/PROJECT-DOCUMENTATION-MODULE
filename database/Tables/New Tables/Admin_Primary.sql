4711b3db26e0e292f900f8c45bba014d99657a3d
Mirza Adnan Baig Nusratullah <118421209+MAdnanB@users.noreply.github.com>
2023-02-28 00:36:53 +0530

    Adnan unknown

--- database/Tables/New Tables/Admin_Primary.sql
+++ database/Tables/New Tables/Admin_Primary.sql
@@ -1,8 +1,8 @@
 create table Admin_Primary
  (
  Admin_ID int(10) primary key auto_increment,
- Email varchar(50) not null unique key,
- Pwd varchar(20) not null,
+ Email varchar(100) not null unique key,
+ Pwd varchar(50) not null,
  Last_Updated_Date datetime default now(),
  Ext_Col_1 varchar(100),
  Ext_Col_2 varchar(100),
